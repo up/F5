e9d78e97a939c1e45788db42e2eff6f00371989a
Uli Preuss <me@ulipreuss.eu>
2012-10-28 22:40:33 +0100

    merge f5.js into bookmarklet

--- bookmarklet.source.js
+++ bookmarklet.source.js
@@ -1,8 +1,8 @@
-(function () {
+(function (win) {
 
   var
     timer = 1000,
-    title = 'F5 :: ' + document.title,
+    title = '[F5] ' + document.title,
     src = window.location.href,
     F5win, F5WinHead, F5WinBody,
     iframe, script, getDimentions, setDimentions
@@ -35,11 +35,156 @@
 
   };
 
+  function F5(win) {
+	  var 
+	    timer, len, checksum, compare, get, activate, deactivate, add, getScrollXY,
+	    checksums = [],
+	    positions = [],
+	    count = 0,
+	    pending = false,
+	    running = false,
+	    interval = 1000,
+	    method = "header",
+	    files = []
+	  ;
+
+	  compare = function() {
+	    pending = true;
+
+	    if (checksums[count] === undefined) {
+	      checksums[count] = checksum;
+	    } else if (checksums[count] !== checksum) {
+		    checksums[count] = checksum;
+		    var iframe = document.getElementById("iframe1");
+		    var positions = getScrollXY();
+		    iframe.contentWindow.location.reload(true);
+		    iframe.onload = function () {
+				    iframe.contentWindow.scrollTo(positions[0], positions[1]);
+				};
+	    }
+
+	    count++;
+
+	    if (count < len) {
+	      get();
+	    } else {
+	      pending = false;
+	      count = 0;
+	    }
+	  };
+
+		getScrollXY = function() {
+		    var 
+		      posX = 0, posY = 0,
+				  iframe = document.getElementById("iframe1"),
+	        iWin = iframe.contentWindow,
+	        iDoc = iWin.document,
+	        iBody = iDoc.body,
+	        iElem = iDoc.documentElement
+	      ;
+
+		    if( typeof( iWin.pageYOffset ) == 'number' ) {
+		        //Netscape compliant
+		        posY = iWin.pageYOffset;
+		        posX = iWin.pageXOffset;
+		    } else if( iBody && ( iBody.scrollLeft || iBody.scrollTop ) ) {
+		        //DOM compliant
+		        posY = iBody.scrollTop;
+		        posX = iBody.scrollLeft;
+		    } else if( iElem && ( iElem.scrollLeft || iElem.scrollTop ) ) {
+		        //IE6 standards compliant mode
+		        posY = iElem.scrollTop;
+		        posX = iElem.scrollLeft;
+		    }
+		    return [ posX, posY ];
+		};
+
+	  get = function() {
+	    var req = new XMLHttpRequest();    
+	    req.onreadystatechange = function() {
+	      if (req.readyState === 4) {
+	        switch (method) {
+	          case "header":
+	            if (req.status === 200) {
+	              checksum = req.getResponseHeader("Last-Modified").toString();
+	              compare();
+	            } 
+	            break;  
+	          case "length":
+	            checksum = req.responseText.toString().length;
+	            compare();
+	            break;  
+	          case "content":
+	            checksum = req.responseText.toString();
+	            compare();
+	            break;
+	        }
+	      }
+	    };
+	    req.open("GET", files[count] + "?" + new Date().getTime(), true); 
+	    req.send(null);
+	  };
+
+	  activate = function() {
+	    files = getfiles();
+	    len = files.length;
+	    timer = win.setInterval(function() {
+	      if (!pending) {
+	        get();
+	      }
+	    }, interval);
+	    running = true;
+	  };
+
+	  deactivate = function() {
+	    win.clearInterval(timer);
+	    running = true;
+	  };
+
+	  add = function(file) {
+	    files.push(file);	
+	  };
+
+	  getfiles = function() {
+	    var 
+	      i, href, src,
+	      iframe = document.getElementById("iframe1"),
+	      doc = iframe.contentWindow.document,
+	      stylesheets = doc.styleSheets,
+	      scripts = doc.getElementsByTagName('script')
+	    ;
+	    for (i=0; i<stylesheets.length; i++) {
+	      href = stylesheets[i].href;
+	      add(href);        
+	    }    
+	    for (i=0; i<scripts.length; i++) {
+	      src = scripts[i].src;
+	      if(src !== '' && src.substring(src.length-5) !== 'f5.js') {
+	        add(src);        
+	      }
+	    }    
+	    return files;
+	  };
+
+		win.setTimeout(function() {
+		  activate();
+	  }, interval);
+
+		win['F5'] = {};
+		win['F5']['activate'] = activate;
+		win['F5']['deactivate'] = deactivate;
+		win['F5']['running'] = running;
+
+  }
+
+
   if (document.getElementById('iframe1') === null) {
 
-    F5win = window.open(),
-    F5WinHead = F5win.document.head,
-    F5WinBody = F5win.document.body,
+		var hasInnerText = (document.getElementsByTagName("body")[0].innerText != undefined) ? true : false;
+
+    F5win = window.open();
+    F5WinHead = F5win.document.head;
+    F5WinBody = F5win.document.body;
 
     F5WinHead.innerHTML = '<title>' + title + '</title><style>body{margin:0;overflow:hidden}</style>';
 
@@ -50,7 +195,12 @@
 
     script = document.createElement('script');
     script.type = "text/javascript";
-    script.src = 'http://ulipreuss.eu/test/F5/f5.js';
+		if(!hasInnerText){
+		    script.textContent = '(' + F5.toString() + '(window));';
+		} else {
+		    script.innerText = '(' + F5.toString() + '(window));';
+		}  
+    // script.src = 'http://ulipreuss.eu/test/F5/f5.js';
     F5WinBody.appendChild(script);
 
     setDimentions();
@@ -80,4 +230,4 @@
     // document.write ALWAYS starts a new page unless there is one currently being loaded.
     //window.history.back();
   }
-})();
+})(window);
---
+++ test/assets/test.coffee
@@ -0,0 +1,11 @@
+mobyDick = "
+   Call me <b>Ishmael</b>. Some years ago --
+   never mind how long precisely -- 
+   having little or no money in my purse, 
+   and nothing particular to interest me 
+   on shore, I thought I would sail
+   about a little and see the watery part 
+   of the World...
+"
+p = document.getElementsByTagName("p")[0]
+p.innerHTML = mobyDick
