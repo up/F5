295acd6be149ff148eb9a95ed61515c3dbfebcb6
Uli Preuss <me@ulipreuss.eu>
2012-10-28 22:57:45 +0100

    some cleanup

--- bookmarklet.source.js
+++ bookmarklet.source.js
@@ -4,8 +4,7 @@
     timer = 1000,
     title = '[F5] ' + document.title,
     src = window.location.href,
-    F5win, F5WinHead, F5WinBody,
-    iframe, script, getDimentions, setDimentions
+    F5win, F5WinHead, F5WinBody, iframe, script, getDimentions, setDimentions
   ;
 
   getDimentions = function () {
@@ -32,155 +31,154 @@
     ;
     iframe.style.width = dim[0];
     iframe.style.height = dim[1];
-
   };
 
   function F5(win) {
-	  var 
-	    timer, len, checksum, compare, get, activate, deactivate, add, getScrollXY,
-	    checksums = [],
-	    positions = [],
-	    count = 0,
-	    pending = false,
-	    running = false,
-	    interval = 1000,
-	    method = "header",
-	    files = []
-	  ;
+    var
+      timer, len, checksum, compare, get, activate, deactivate, add, getScrollXY, 
+      checksums = [],
+      positions = [],
+      count = 0,
+      pending = false,
+      running = false,
+      interval = 1000,
+      method = "header",
+      files = []
+    ;
 
-	  compare = function() {
-	    pending = true;
+    compare = function () {
+	    var iframe, positions;
+      pending = true;
 
-	    if (checksums[count] === undefined) {
-	      checksums[count] = checksum;
-	    } else if (checksums[count] !== checksum) {
-		    checksums[count] = checksum;
-		    var iframe = document.getElementById("iframe1");
-		    var positions = getScrollXY();
-		    iframe.contentWindow.location.reload(true);
-		    iframe.onload = function () {
-				    iframe.contentWindow.scrollTo(positions[0], positions[1]);
-				};
-	    }
+      if (checksums[count] === undefined) {
+        checksums[count] = checksum;
+      } else if (checksums[count] !== checksum) {
+        checksums[count] = checksum;
+        iframe = document.getElementById("iframe1");
+        positions = getScrollXY();
+        iframe.contentWindow.location.reload(true);
+        iframe.onload = function () {
+          iframe.contentWindow.scrollTo(positions[0], positions[1]);
+        };
+      }
 
-	    count++;
+      count++;
 
-	    if (count < len) {
-	      get();
-	    } else {
-	      pending = false;
-	      count = 0;
-	    }
-	  };
+      if (count < len) {
+        get();
+      } else {
+        pending = false;
+        count = 0;
+      }
+    };
 
-		getScrollXY = function() {
-		    var 
-		      posX = 0, posY = 0,
-				  iframe = document.getElementById("iframe1"),
-	        iWin = iframe.contentWindow,
-	        iDoc = iWin.document,
-	        iBody = iDoc.body,
-	        iElem = iDoc.documentElement
-	      ;
+    getScrollXY = function () {
+      var
+        posX = 0,
+        posY = 0,
+        iframe = document.getElementById("iframe1"),
+        iWin = iframe.contentWindow,
+        iDoc = iWin.document,
+        iBody = iDoc.body,
+        iElem = iDoc.documentElement
+      ;
 
-		    if( typeof( iWin.pageYOffset ) == 'number' ) {
-		        //Netscape compliant
-		        posY = iWin.pageYOffset;
-		        posX = iWin.pageXOffset;
-		    } else if( iBody && ( iBody.scrollLeft || iBody.scrollTop ) ) {
-		        //DOM compliant
-		        posY = iBody.scrollTop;
-		        posX = iBody.scrollLeft;
-		    } else if( iElem && ( iElem.scrollLeft || iElem.scrollTop ) ) {
-		        //IE6 standards compliant mode
-		        posY = iElem.scrollTop;
-		        posX = iElem.scrollLeft;
-		    }
-		    return [ posX, posY ];
-		};
+      if (typeof(iWin.pageYOffset) == 'number') {
+        //Netscape compliant
+        posY = iWin.pageYOffset;
+        posX = iWin.pageXOffset;
+      } else if (iBody && (iBody.scrollLeft || iBody.scrollTop)) {
+        //DOM compliant
+        posY = iBody.scrollTop;
+        posX = iBody.scrollLeft;
+      } else if (iElem && (iElem.scrollLeft || iElem.scrollTop)) {
+        //IE6 standards compliant mode
+        posY = iElem.scrollTop;
+        posX = iElem.scrollLeft;
+      }
+      return [posX, posY];
+    };
 
-	  get = function() {
-	    var req = new XMLHttpRequest();    
-	    req.onreadystatechange = function() {
-	      if (req.readyState === 4) {
-	        switch (method) {
-	          case "header":
-	            if (req.status === 200) {
-	              checksum = req.getResponseHeader("Last-Modified").toString();
-	              compare();
-	            } 
-	            break;  
-	          case "length":
-	            checksum = req.responseText.toString().length;
-	            compare();
-	            break;  
-	          case "content":
-	            checksum = req.responseText.toString();
-	            compare();
-	            break;
-	        }
-	      }
-	    };
-	    req.open("GET", files[count] + "?" + new Date().getTime(), true); 
-	    req.send(null);
-	  };
+    get = function () {
+      var req = new XMLHttpRequest();
+      req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+          switch (method) {
+          case "header":
+            if (req.status === 200) {
+              checksum = req.getResponseHeader("Last-Modified").toString();
+              compare();
+            }
+            break;
+          case "length":
+            checksum = req.responseText.toString().length;
+            compare();
+            break;
+          case "content":
+            checksum = req.responseText.toString();
+            compare();
+            break;
+          }
+        }
+      };
+      req.open("GET", files[count] + "?" + new Date().getTime(), true);
+      req.send(null);
+    };
 
-	  activate = function() {
-	    files = getfiles();
-	    len = files.length;
-	    timer = win.setInterval(function() {
-	      if (!pending) {
-	        get();
-	      }
-	    }, interval);
-	    running = true;
-	  };
+    activate = function () {
+      files = getfiles();
+      len = files.length;
+      timer = win.setInterval(function () {
+        if (!pending) {
+          get();
+        }
+      }, interval);
+      running = true;
+    };
 
-	  deactivate = function() {
-	    win.clearInterval(timer);
-	    running = true;
-	  };
+    deactivate = function () {
+      win.clearInterval(timer);
+      running = true;
+    };
 
-	  add = function(file) {
-	    files.push(file);	
-	  };
+    add = function (file) {
+      files.push(file);
+    };
 
-	  getfiles = function() {
-	    var 
-	      i, href, src,
-	      iframe = document.getElementById("iframe1"),
-	      doc = iframe.contentWindow.document,
-	      stylesheets = doc.styleSheets,
-	      scripts = doc.getElementsByTagName('script')
-	    ;
-	    for (i=0; i<stylesheets.length; i++) {
-	      href = stylesheets[i].href;
-	      add(href);        
-	    }    
-	    for (i=0; i<scripts.length; i++) {
-	      src = scripts[i].src;
-	      if(src !== '' && src.substring(src.length-5) !== 'f5.js') {
-	        add(src);        
-	      }
-	    }    
-	    return files;
-	  };
+    getfiles = function () {
+      var
+        i, href, src, iframe = document.getElementById("iframe1"),
+        doc = iframe.contentWindow.document,
+        stylesheets = doc.styleSheets,
+        scripts = doc.getElementsByTagName('script')
+      ;
+      for (i = 0; i < stylesheets.length; i++) {
+        href = stylesheets[i].href;
+        add(href);
+      }
+      for (i = 0; i < scripts.length; i++) {
+        src = scripts[i].src;
+        if (src !== '' && src.substring(src.length - 5) !== 'f5.js') {
+          add(src);
+        }
+      }
+      return files;
+    };
 
-		win.setTimeout(function() {
-		  activate();
-	  }, interval);
+    win.setTimeout(function () {
+      activate();
+    }, interval);
 
-		win['F5'] = {};
-		win['F5']['activate'] = activate;
-		win['F5']['deactivate'] = deactivate;
-		win['F5']['running'] = running;
+    win['F5'] = {};
+    win['F5']['activate'] = activate;
+    win['F5']['deactivate'] = deactivate;
+    win['F5']['running'] = running;
 
   }
 
-
   if (document.getElementById('iframe1') === null) {
 
-		var hasInnerText = (document.getElementsByTagName("body")[0].innerText != undefined) ? true : false;
+    var hasInnerText = (document.getElementsByTagName("body")[0].innerText != undefined) ? true : false;
 
     F5win = window.open();
     F5WinHead = F5win.document.head;
@@ -195,12 +193,12 @@
 
     script = document.createElement('script');
     script.type = "text/javascript";
-		if(!hasInnerText){
-		    script.textContent = '(' + F5.toString() + '(window));';
-		} else {
-		    script.innerText = '(' + F5.toString() + '(window));';
-		}  
-    // script.src = 'http://ulipreuss.eu/test/F5/f5.js';
+    if (!hasInnerText) {
+      script.textContent = '(' + F5.toString() + '(window));';
+    } else {
+      script.innerText = '(' + F5.toString() + '(window));';
+    }
+
     F5WinBody.appendChild(script);
 
     setDimentions();
@@ -230,4 +228,5 @@
     // document.write ALWAYS starts a new page unless there is one currently being loaded.
     //window.history.back();
   }
+
 })(window);
