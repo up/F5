74d132aabc8181ae75122e36ab3a750d48ed4050
Uli Preuss <me@ulipreuss.eu>
2012-10-31 15:40:38 +0100

    changed test files

--- test/assets/test.coffee
+++ test/assets/test.coffee
@@ -1,11 +1,11 @@
 mobyDick = "
-   Call me <b>Ishmael</b>. Some years ago --
+   Call me Ishmael. Some years ago --
    never mind how long precisely -- 
    having little or no money in my purse, 
    and nothing particular to interest me 
    on shore, I thought I would sail
    about a little and see the watery part 
-   of the World...
+   of the World ...
 "
 p = document.getElementsByTagName("p")[0]
 p.innerHTML = mobyDick
--- test/assets/test.js
+++ test/assets/test.js
@@ -2,7 +2,7 @@
 (function() {
   var mobyDick, p;
 
-  mobyDick = "   Call me <b>Ishmael</b>. Some years ago --   never mind how long precisely --    having little or no money in my purse,    and nothing particular to interest me    on shore, I thought I would sail   about a little and see the watery part    of the World...";
+  mobyDick = "   Call me Ishmael. Some years ago --   never mind how long precisely --    having little or no money in my purse,    and nothing particular to interest me    on shore, I thought I would sail   about a little and see the watery part    of the World ...";
 
   p = document.getElementsByTagName("p")[0];
 
