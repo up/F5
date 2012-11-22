8bae96b22a4af1957cbbc94c736841df4f34ee7c
Uli Preuss <me@ulipreuss.eu>
2012-11-22 16:39:26 +0100

    add tabs for pages with more than 10 files

--- bookmarklet.source.js
+++ bookmarklet.source.js
@@ -37,13 +37,13 @@
 
   function F5(win, iframeId, method, interval) {
     var
-      timer, len, checksum, compare, get, getfiles, activate, deactivate, add, getScrollXY, _SB_, checksums = [],
+      timer, len, checksum, compare, get, getfiles, activate, deactivate, add, addTabs, getScrollXY, _SB_, checksums = [],
       count = 0,
       pending = false,
       running = false,
       files = []
-   ;
-  
+    ;
+
     _SB_ = function (files) {
       var
         position = {
@@ -65,16 +65,18 @@
 
       function setBorderRadius(y, x) {
         var radius = '5px;';
-        return '-webkit-border-' + y + '-' + x + '-radius: ' + radius + 
-               '-moz-border-radius-' + y + x + ': ' + radius + 
-               'border-' + y + '-' + x + '-radius: ' + radius;
+        return 
+          '-webkit-border-' + y + '-' + x + '-radius: ' + radius + 
+          '-moz-border-radius-' + y + x + ': ' + radius + 
+          'border-' + y + '-' + x + '-radius: ' + radius;
       }
 
       function setBoxShadow() {
         var boxshadow = '-2px 2px 10px #222;';
-        return '-webkit-box-shadow: ' + boxshadow + 
-               '-moz-box-shadow: ' + boxshadow + 
-               'box-shadow: ' + boxshadow;
+        return 
+          '-webkit-box-shadow: ' + boxshadow + 
+          '-moz-box-shadow: ' + boxshadow + 
+          'box-shadow: ' + boxshadow;
       }
 
       function newDiv(id) {
@@ -119,7 +121,7 @@
       function refreshLayout() {
         contentWidth = SBWrapper.offsetWidth + 15;
         // Convert % manual to %25 for bookmarklet ##############################################
-        contentWidth += 10 - (contentWidth %25 10); 
+        contentWidth += 10 - (contentWidth %25 10);
         SBWrapper.style.width = contentWidth + labelWidth + "px";
         SBWrapper.style.right = -contentWidth + "px";
         SBContentWrapper.style.width = contentWidth - labelWidth + 7 + "px";
@@ -137,7 +139,8 @@
       function toggleAll() {
 
         SBContent = document.querySelector('#SBContent');
-        var 
+        
+        var
           inputs = SBContent.getElementsByTagName('input'),
           i = 0
         ;
@@ -153,13 +156,18 @@
           if (inputs[i].checked) {
             inputs[i].nextSibling.style.color = "#cea500";
           } else {
-            if (/\.css/.test(ext)) {
-              inputs[i].nextSibling.style.color = "#DDD";
-            } else if (/\.js/.test(ext)) {
-              inputs[i].nextSibling.style.color = "#FFF";
+            if(inputs.length > 10) {
+		          if (/\.css/.test(ext)) {
+	              inputs[i].nextSibling.style.color = "#DDD";
+	            } else if (/\.js/.test(ext)) {
+	              inputs[i].nextSibling.style.color = "#FFF";
+	            } else {
+	              inputs[i].nextSibling.style.color = "#FFFFCC"; // images
+	            }
             } else {
-             inputs[i].nextSibling.style.color = "#FFFFCC"; // images
+	            inputs[i].nextSibling.style.color = "#DDD";
             }
+
           }
         }
 
@@ -167,9 +175,9 @@
 
       function createSidebarContent(files) {
 
-        var 
+        var
           i = 0,
-          span, input, label, select, options, option, para, ext
+          div, span, input, label, select, options, option, para, ext, tabnum
         ;
 
         function addElement(tagname, parent) {
@@ -179,9 +187,18 @@
         SBContent.innerHTML = '';
         SBTitle = document.querySelector('#SBTitle');
 
-        if (files.length > 10) {
+        var display = files.length > 10 ? 'tabs' : 'list';
+
+        if (display === 'tabs') {
           SBContent.style.width = '500px';
+
+          jtabs.create(
+	          ['CSS', ''], 
+	          ['JavaScript', ''], 
+	          ['Images', '']
+	        ).appendTo(SBContent);
         }
+        var tabs = document.querySelectorAll('#tabContent > div');
         for (; i < files.length; i += 1) {
           span = document.createElement('span');
           span.style.cssText = "display:inline-block;white-space:nowrap;";
@@ -196,32 +213,46 @@
           label.style.cssText = "cursor:pointer; margin-right:12px";
           ext = files[i][1].toLowerCase();
           if (/\.css/.test(ext)) {
-            label.style.cssText += "color:#DDD;";
+            if (display === 'list') {
+              label.style.cssText += "color:#DDD;";
+            }
+            tabnum = 0;
           } else if (/\.js/.test(ext)) {
-            label.style.cssText += "color:#FFF";
+            if (display === 'list') {
+              label.style.cssText += "color:#FFF";
+            }
+            tabnum = 1;
           } else {
-           label.style.cssText += "color:#FFFFCC"; // images
+            if (display === 'list') {
+              label.style.cssText += "color:#FFFFCC"; // images
+            }
+            tabnum = 2;
           }
 
           label.innerHTML = files[i][1];
 
           span.appendChild(input);
           span.appendChild(label);
-          SBContent.appendChild(span);
 
-          if (files.length <= 10 && i < files.length - 1) {
-            addElement('br', SBContent);
+          if (display === 'list') {
+            SBContent.appendChild(span);
+            if (i < files.length - 1) {
+              addElement('br', SBContent);
+            }
+          } else {
+            tabs[tabnum].appendChild(span);
           }
         }
 
         para = document.createElement('p');
         para.style.cssText = "text-align:right; margin: 10px 0";
-        para.innerHTML = '' +
-          '<a href="https://github.com/up/F5" ' +
+        para.innerHTML = '' + 
+          '<a href="http://up.github.com/F5/" ' + 
           'style="' + 
-          '  background-color: #666;display:inline-block;padding:2px 4px;' +
-          '  color:#E8E8E8;text-decoration:none;font-size: 0.7em; ' +
-          '">F5 @ github</a>';
+          '  background-color: #666;display:inline-block;padding:2px 4px;' + 
+          '  color:#E8E8E8;text-decoration:none;font-size: 0.7em; ' + 
+          '">F5 @ github</a>'
+        ;
         SBContent.appendChild(para);
 
         select = document.createElement('select');
@@ -231,7 +262,7 @@
         options = [
           ["Last-Modified", "Last-Modified"],
           ["Content-Length", "Content-Length"]
-          //["content", "content"]
+          //["content", "content"]];
         ];
 
         for (i = 0; i < options.length; i += 1) {
@@ -273,8 +304,11 @@
       SB = newDiv(null);
       SB.style.cssText = '' + 
         'font-family:arial;' + 
-        'position:absolute;right:' + position.right + 'px;top:' + position.top + 'px;' + 
-        'z-index: 1000;';
+        'position:absolute;' +
+        'right:' + position.right + 'px;' +
+        'top:' + position.top + 'px;' + 
+        'z-index: 1000;'
+      ;
       body.appendChild(SB);
 
       SBWrapper = newDiv('SBWrapper');
@@ -282,7 +316,8 @@
       SBWrapper.onmouseout = hide;
       SBWrapper.style.cssText = '' + 
         'position:absolute;right:0;top:0;' + 
-        'visibility:hidden;';
+        'visibility:hidden;'
+      ;
       SB.appendChild(SBWrapper);
 
       SBLabel = newDiv(null);
@@ -292,7 +327,8 @@
         setBorderRadius('bottom', 'left') + 
         setBoxShadow() + 
         'vertical-align:top;' + 
-        'background-color:#222;';
+        'background-color:#222;'
+      ;
       SBLabel.innerHTML = label;
       SBWrapper.appendChild(SBLabel);
 
@@ -302,7 +338,8 @@
         setBorderRadius('bottom', 'left') + 
         setBoxShadow() + 
         'padding-top:17px;padding-left:20px;' + 
-        'background-color:#222;';
+        'background-color:#222;'
+      ;
       SBContentWrapper.innerHTML = title + content;
       SBWrapper.appendChild(SBContentWrapper);
 
@@ -384,7 +421,7 @@
             case "Content-Length":
               checksum = req.getResponseHeader("Content-Length");
               break;
-            /*
+              /*
             case "content":
               checksum = req.responseText.toString();
               break;
@@ -409,7 +446,7 @@
         doc = iframe.contentWindow.document,
         stylesheets = doc.styleSheets,
         scripts = doc.getElementsByTagName('script'),
-        imgs =  doc.getElementsByTagName('img'),
+        imgs = doc.getElementsByTagName('img'),
         img_ext = 'png gif svg jpg jpeg',
         ext, hash = {}
       ;
@@ -429,19 +466,20 @@
       for (i = 0; i < imgs.length; i++) {
         src = imgs[i].src;
         ext = src.substring(src.lastIndexOf('.') + 1).toLowerCase();
-        if (src !== '' && img_ext.indexOf(ext) !== -1) {
-            hash[src] = true;
-          }
+        if (src !== '' && img_ext.indexOf(ext) !== - 1) {
+          hash[src] = true;
         }
+      }
 
-        // push the unique image URLs
-        for (url in hash) {
-          add(url);
-        }
+      // push the unique image URLs
+      for (url in hash) {
+        add(url);
+      }
       return files;
     };
 
     activate = function () {
+      addTabs(win);
       files = getfiles();
       _SB_(files);
       len = files.length;
@@ -463,16 +501,135 @@
         file,
         file.substring(file.lastIndexOf('/') + 1),
         false
-      ]);
+        ]);
     };
 
-      win.setTimeout(function () {
+    addTabs = function (win) {
+
+      var
+        tab_container, iframe = document.getElementById(iframeId),
+        doc = document
+      ;
+
+      function newElement(name) {
+        return doc.createElement(name);
+      }
+
+      function getAll(selector) {
+        return doc.querySelectorAll(selector);
+      }
+
+      function toggleTabs(tab) {
+        var
+          i, lis = getAll("#tabs li"),
+          tas = getAll("#tabs #tabContent div")
+        ;
+        for (i = 0; i < lis.length; i++) {
+          lis[i].className = "";
+        }
+        for (i = 0; i < tas.length; i++) {
+          tas[i].style.display = 'none';
+        }
+        tab.className = "selected";
+        tas[parseInt(tab.id.substring(3), 10) - 1].style.display = 'block';
+      }
+
+      function createListItem(id, title, sel) {
+        var li = newElement('li');
+        li.innerHTML = title;
+        li.id = id;
+        li.onclick = function () {
+          toggleTabs(this);
+        };
+        if (sel) {
+          li.className = "selected";
+        }
+        return li;
+      }
+
+      function addStyleRules(rules) {
+        var
+          head = doc.getElementsByTagName('head')[0],
+          style = newElement('style')
+        ;
+        style.type = 'text/css';
+        if (style.styleSheet) {
+          style.styleSheet.cssText = rules;
+        } else {
+          style.appendChild(doc.createTextNode(rules));
+        }
+        head.appendChild(style);
+      }
+
+      function createTabContentWrapper(content, hide) {
+        var div = newElement('div');
+        div.innerHTML = content;
+        if (hide) {
+          div.style.display = 'none';
+        }
+        return div;
+      }
+
+      function appendTo(target) {
+        target.appendChild(tab_container);
+      }
+
+      function create() {
+        var
+          args = arguments,
+          len = args.length,
+          tab_ul = newElement('ul'),
+          tab_content = newElement('div')
+        ;
+        tab_container = newElement('div');
+        tab_container.id = "tabs";
+        tab_ul.id = "sidebarTabs";
+        tab_container.appendChild(tab_ul);
+        for (i = 0; i < len; i++) {
+          tab_ul.appendChild(createListItem('tab' + (i + 1), args[i][0], i === 0 ? true : false));
+        }
+        tab_content.id = "tabContent";
+        tab_container.appendChild(tab_content);
+        for (i = 0; i < len; i++) {
+          tab_content.appendChild(createTabContentWrapper(args[i][1], i > 0 ? true : false));
+        }
+        return this; // chaining
+      }
+
+      addStyleRules(
+	      '#tabs{ margin:10px 0 0; }' + 
+	      '#tabs ul { ' +
+	      '  list-style:none; display:block; overflow:hidden; ' +
+	      '  margin:0; padding:0; position: relative; top:1px;' +
+	      '}' + 
+	      '#tabs li { ' + 
+	      '  float:left; border:1px solid #444; padding:5px 8px; ' +
+	      '  cursor:pointer; border-bottom:none; margin-right:4px;' + 
+	      '  font-family:verdana; font-size:.8em; font-weight:bold; color:#AAA; ' + 
+	      '}' + 
+	      '#tabs .selected{ ' +
+	      '  background-color:#444; color:#cea500; font-weight: bold;' +
+	      '}' + 
+	      '#tabs #tabContent div { ' +
+	      '  padding:10px; color:#EEE; background-color:#444; overflow:hidden;' +
+	      '}'
+	    );
+
+      win['jtabs'] = {};
+      win['jtabs']['create'] = create;
+      win['jtabs']['appendTo'] = appendTo;
+
+    };
+
+
+    win.setTimeout(function () {
       activate();
 
       SBContent.onclick = function () {
-        var 
-          inputs = SBContent.getElementsByTagName('input'), 
-          i = 0, ext
+        var
+          inputs = SBContent.getElementsByTagName('input'),
+          i = 0,
+          ext
         ;
         for (; i < inputs.length; i += 1) {
           files[i][2] = inputs[i].checked;
@@ -484,8 +641,8 @@
             if (/\.css/.test(ext)) {
               inputs[i].nextSibling.style.color = "#DDD";
             } else if (/\.js/.test(ext)) {
-                inputs[i].nextSibling.style.color = "#FFF";
-              } else {
+              inputs[i].nextSibling.style.color = "#FFF";
+            } else {
               inputs[i].nextSibling.style.color = "#FFFFCC"; // images
             }
           }
@@ -502,60 +659,57 @@
 
     }, 100);
 
-
   }
 
-  
-    hasInnerText = (document.getElementsByTagName("body")[0].innerText !== undefined) ? true : false;
-    scriptText = '(' + F5.toString() + '(window, "' + iframeId + '", "' + method + '", ' + interval + '));';
+  hasInnerText = (document.getElementsByTagName("body")[0].innerText !== undefined) ? true : false;
+  scriptText = '(' + F5.toString() + '(window, "' + iframeId + '", "' + method + '", ' + interval + '));';
 
-    F5win = window.open();
-    F5WinHead = F5win.document.head;
-    F5WinBody = F5win.document.body;
+  F5win = window.open();
+  F5WinHead = F5win.document.head;
+  F5WinBody = F5win.document.body;
 
-    F5WinHead.innerHTML = '' + 
-      '<title>' + title + '</title>' +
-      '<style>' +
-      'body{ margin:0; overflow:hidden;} ' +
-      'iframe{border:none} ' +
-      'select, button {margin-left:7px;margin-top:3px;font-size:0.8em;} ' +
-      'button {margin-left:8px;} ' +
-      'option {padding: 2px 3px 0px;}' +
-      '</style>';
+  F5WinHead.innerHTML = '' + 
+    '<title>' + title + '</title>' + 
+    '<style>' + 
+    'body{ margin:0; overflow:hidden;} ' + 
+    'iframe{border:none} ' + 
+    'select, button {margin-left:7px;margin-top:3px;font-size:0.8em;} ' + 
+    'button {margin-left:8px;} ' + 
+    'option {padding: 2px 3px 0px;}' + 
+    '</style>'
+  ;
 
-    iframe = document.createElement('iframe');
-    iframe.id = iframeId;
-    iframe.src = src;
-    F5WinBody.appendChild(iframe);
+  iframe = document.createElement('iframe');
+  iframe.id = iframeId;
+  iframe.src = src;
+  F5WinBody.appendChild(iframe);
 
-    script = document.createElement('script');
-    script.type = "text/javascript";
-    if (!hasInnerText) {
-      script.textContent = scriptText;
-    } else {
-      script.innerText = scriptText;
+  script = document.createElement('script');
+  script.type = "text/javascript";
+  if (!hasInnerText) {
+    script.textContent = scriptText;
+  } else {
+    script.innerText = scriptText;
+  }
+
+  var completeIVal = setInterval(function () {
+    if (iframe.contentWindow.document.readyState === 'complete') {
+      F5WinBody.appendChild(script);
+      clearInterval(completeIVal);
     }
+  }, 100);
 
-    var completeIVal = setInterval(function(){
-      if(iframe.contentWindow.document.readyState === 'complete') {
-        F5WinBody.appendChild(script);
-        clearInterval(completeIVal);
-      }
-    }, 100);
+  setDimentions();
 
-    setDimentions();
+  if (window.focus) {
+    F5win.focus();
+  }
 
-    if (window.focus) {
-      F5win.focus();
-    }
+  F5win.onresize = setDimentions;
 
-    F5win.onresize = setDimentions;
+  document.close();
 
-    document.close();
-
-    F5win.F5 = {};
-    F5win.F5.activate = activate;
-    F5win.F5.deactivate = deactivate;
-    F5win.F5.running = running;
+  F5win.F5 = {};
+  //F5win.F5.running = running;
 
 })(window);
